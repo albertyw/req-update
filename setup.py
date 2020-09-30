fd1412962af6a080a00709ad6e3ec471e02a2c9a
Albert Wang <git@albertyw.com>
2020-09-29 19:57:33 -0700

    Fix lint

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-about: Dict[str, str]  = {}
+about: Dict[str, str] = {}
 with open(path.join(here, 'pip_update', '__version__.py')) as f:
     exec(f.read(), about)
 
