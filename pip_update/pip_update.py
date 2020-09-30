fc520e36bf0f04168b03579b718e3d2bfde71210
Albert Wang <git@albertyw.com>
2020-09-29 22:43:11 -0700

    Add pip_update outline

--- pip_update/pip_update.py
+++ pip_update/pip_update.py
@@ -1,9 +1,42 @@
 from __future__ import annotations
 import subprocess
-from typing import List
+from typing import List, Tuple
 
 
 def main() -> None:
+    """ Update all dependencies """
+    check_repository_cleanliness()
+    create_branch()
+    update_dependencies()
+
+
+def check_repository_cleanliness() -> None:
+    """ Check that the repository is ready for updating dependencies """
+    pass
+
+
+def create_branch() -> None:
+    """ Create a new branch for committing dependency updates """
+    pass
+
+
+def update_dependencies() -> None:
+    """ Update and commit a list of dependency updates """
+    pass
+
+
+def get_pip_outdated() -> List[Tuple[str, str]]:
+    """ Get a list of outdated pip packages """
+    pass
+
+
+def write_dependency_update(dependency: str, version: str) -> None:
+    """ Given a dependency, update it to a given version """
+    pass
+
+
+def commit_dependency_update(dependency: str, version: str) -> None:
+    """ Create a commit with a dependency update """
     pass
 
 
