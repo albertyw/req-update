590510fdacdd5976362e3276b504a6ea11c6d7bb
Albert Wang <git@albertyw.com>
2020-09-29 19:53:36 -0700

    Backfill type annotations

--- pip_update/pip_update.py
+++ pip_update/pip_update.py
@@ -1,11 +1,12 @@
 import subprocess
+from typing import List
 
 
-def main():
+def main() -> None:
     pass
 
 
-def execute_shell(command):
+def execute_shell(command: List[str]) -> subprocess.CompletedProcess[bytes]:
     result = subprocess.run(
         command,
         capture_output=True,
