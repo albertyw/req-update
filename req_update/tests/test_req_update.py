c21ee11bcba34e4c2f725811fea7a6d38296d410
Albert Wang <git@albertyw.com>
2020-10-03 22:16:43 -0700

    Add test stubs

--- req_update/tests/test_req_update.py
+++ req_update/tests/test_req_update.py
@@ -4,6 +4,13 @@ from unittest.mock import MagicMock, Mock, patch
 from req_update import req_update
 
 
+class TestMain(unittest.TestCase):
+    @patch('req_update.req_update.check_repository_cleanliness')
+    def test_main(self, mock_check: Mock) -> None:
+        req_update.main()
+        self.assertTrue(mock_check.called)
+
+
 class TestCheckRepositoryCleanliness(unittest.TestCase):
     @patch('req_update.req_update.execute_shell')
     def test_clean(self, mock_execute_shell: Mock) -> None:
@@ -17,6 +24,31 @@ class TestCheckRepositoryCleanliness(unittest.TestCase):
         )
         with self.assertRaises(RuntimeError):
             req_update.check_repository_cleanliness()
+
+
+class TestCreateBranch(unittest.TestCase):
+    def test_create_branch(self) -> None:
+        req_update.create_branch()
+
+
+class TestUpdateDependencies(unittest.TestCase):
+    def test_update_dependencies(self) -> None:
+        req_update.update_dependencies()
+
+
+class TestGetPipOutdated(unittest.TestCase):
+    def test_get_pip_outdated(self) -> None:
+        req_update.get_pip_outdated()
+
+
+class TestWriteDependencyUpdate(unittest.TestCase):
+    def test_write_dependency_update(self) -> None:
+        req_update.write_dependency_update('varsnap', '1.2.3')
+
+
+class TestCommitDependencyUpdate(unittest.TestCase):
+    def test_commit_dependency_update(self) -> None:
+        req_update.commit_dependency_update('varsnap', '1.2.3')
 
 
 class TestExecuteShell(unittest.TestCase):
