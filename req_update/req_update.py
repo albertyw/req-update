4eb8723e6285a50647caa80e955cf1034b9ee9fb
Albert Wang <git@albertyw.com>
2020-10-05 02:03:29 -0700

    Implement get_pip_outdated

--- req_update/req_update.py
+++ req_update/req_update.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import argparse
+import json
 import subprocess
-from typing import List, Tuple
+from typing import Dict, List
 
 VERSION = (0, 0, 1)
 __version__ = '.'.join(map(str, VERSION))
@@ -64,9 +65,12 @@ class ReqUpdate():
         """ Update and commit a list of dependency updates """
         pass
 
-    def get_pip_outdated(self) -> List[Tuple[str, str]]:
+    def get_pip_outdated(self) -> List[Dict[str, str]]:
         """ Get a list of outdated pip packages """
-        pass
+        command = ['pip', 'list', '--outdated', '--format', 'json']
+        result = self.execute_shell(command)
+        outdated: List[Dict[str, str]] = json.loads(result.stdout)
+        return outdated
 
     def write_dependency_update(self, dependency: str, version: str) -> None:
         """ Given a dependency, update it to a given version """
