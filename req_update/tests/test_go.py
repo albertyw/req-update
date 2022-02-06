aef94d85671743e528bfb8fa6ce8ac07951da802
Albert Wang <git@albertyw.com>
2022-02-05 21:25:45 -0500

    Add tests for check_applicable

--- req_update/tests/test_go.py
+++ req_update/tests/test_go.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+import subprocess
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from req_update import go
 
@@ -12,9 +13,34 @@ class TestCheckApplicable(unittest.TestCase):
         self.mock_execute_shell = MagicMock()
         setattr(self.go.util, "execute_shell", self.mock_execute_shell)
 
-    def test(self) -> None:
-        with self.assertRaises(NotImplementedError):
-            self.go.check_applicable()
+    @patch("os.listdir")
+    def test_applicable(self, mock_listdir: MagicMock) -> None:
+        self.mock_execute_shell.return_value = MagicMock()
+        mock_listdir.return_value = ["go.mod", "go.sum"]
+        result = self.go.check_applicable()
+        self.assertTrue(result)
+
+    @patch("os.listdir")
+    def test_no_go(self, mock_listdir: MagicMock) -> None:
+        error = subprocess.CalledProcessError(1, "asdf")
+        self.mock_execute_shell.side_effect = error
+        mock_listdir.return_value = ["go.mod", "go.sum"]
+        result = self.go.check_applicable()
+        self.assertFalse(result)
+
+    @patch("os.listdir")
+    def test_no_go_mod(self, mock_listdir: MagicMock) -> None:
+        self.mock_execute_shell.side_effect = MagicMock()
+        mock_listdir.return_value = ["go.sum"]
+        result = self.go.check_applicable()
+        self.assertFalse(result)
+
+    @patch("os.listdir")
+    def test_no_go_sum(self, mock_listdir: MagicMock) -> None:
+        self.mock_execute_shell.side_effect = MagicMock()
+        mock_listdir.return_value = ["go.mod"]
+        result = self.go.check_applicable()
+        self.assertFalse(result)
 
 
 class TestUpdateInstallDependencies(unittest.TestCase):
