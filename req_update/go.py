2c137e974d96b5ff9b1739088569ee3fdfbfec50
Albert Wang <git@albertyw.com>
2022-02-05 21:21:04 -0500

    Implement check_applicable

--- req_update/go.py
+++ req_update/go.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import os
+import subprocess
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -12,7 +13,17 @@ class Go:
         self.util = util.Util()
 
     def check_applicable(self) -> bool:
-        raise NotImplementedError()
+        command = ["which", "go"]
+        try:
+            self.util.execute_shell(command, True, suppress_output=True)
+        except subprocess.CalledProcessError:
+            # Cannot find go
+            return False
+        files = os.listdir(".")
+        if "go.mod" not in files or "go.sum" not in files:
+            # Cannot find go modules files
+            return False
+        return True
 
     def update_install_dependencies(self) -> bool:
         """
