ec63f2b3b7f6dd7be8d112eba1fe25270705d35e
Albert Wang <git@albertyw.com>
2022-08-20 16:00:30 -0700

    Add tests for executing from CLI

---
+++ req_update/tests/test_cli.py
@@ -0,0 +1,25 @@
+import os
+import pathlib
+import unittest
+
+from req_update import util
+
+
+class TestCLI(unittest.TestCase):
+    def setUp(self) -> None:
+        current = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+        parent = current.parent.resolve()
+        self.req_update_path = parent / 'req_update.py'
+        self.util = util.Util()
+
+    def test_cli(self) -> None:
+        command = ['python3', str(self.req_update_path), '-d']
+        self.util.execute_shell(command, readonly=True)
+
+    def test_shebang(self) -> None:
+        command = [str(self.req_update_path), '-d']
+        self.util.execute_shell(command, readonly=True)
+
+    def test_module(self) -> None:
+        command = ['python3', '-m', 'req_update.req_update']
+        self.util.execute_shell(command, readonly=True)
