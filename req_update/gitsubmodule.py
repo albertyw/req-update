3081afedb168ee9498d2dbacf097ce96384c4770
Albert Wang <git@albertyw.com>
2022-08-21 21:08:49 -0700

    Add git submodule types

--- req_update/gitsubmodule.py
+++ req_update/gitsubmodule.py
@@ -1,4 +1,7 @@
+import datetime
+from pathlib import Path
 import subprocess
+from typing import NamedTuple, Optional
 
 from req_update.util import Updater
 
@@ -15,3 +18,17 @@ class GitSubmodule(Updater):
         except subprocess.CalledProcessError:
             return False
         return len(result.stdout) > 0
+
+
+# TODO: After python 3.7 support is dropped, switch this to a TypedDict
+class Submodule:
+    def __init__(self, path: Path) -> None:
+        self.path: Path = path
+        self.remote_tag: Optional[VersionInfo] = None
+        self.remote_commit: Optional[VersionInfo] = None
+
+
+# TODO: After python 3.7 support is dropped, switch this to a TypedDict
+class VersionInfo(NamedTuple):
+    version_name: str
+    version_date: datetime.datetime
