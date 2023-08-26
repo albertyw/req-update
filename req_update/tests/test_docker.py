be54a8eec6e6e6f49dc3bea325554a119f10ce57
Albert Wang <git@albertyw.com>
2023-08-25 23:43:13 -0400

    Add tests

--- req_update/tests/test_docker.py
+++ req_update/tests/test_docker.py
@@ -1,9 +1,51 @@
 from __future__ import annotations
+import os
+import tempfile
 import unittest
+from unittest.mock import MagicMock
 
 from req_update import docker
 
 
-class TestCheckApplicable(unittest.TestCase):
+class BaseTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.docker = docker.Docker()
+        self.lines = ['FROM debian:10']
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.original_cwd = os.getcwd()
+        os.chdir(self.tempdir.name)
+        with open('Dockerfile', 'w') as handle:
+            handle.write('\n'.join(self.lines))
+        self.mock_warn = MagicMock()
+        setattr(self.docker.util, 'warn', self.mock_warn)
+
+    def tearDown(self) -> None:
+        self.tempdir.cleanup()
+        os.chdir(self.original_cwd)
+
+
+class TestCheckApplicable(BaseTest):
     def test_check(self) -> None:
-        self.assertFalse(docker.Docker().check_applicable())
+        self.assertTrue(self.docker.check_applicable())
+        os.chdir('/')
+        self.assertFalse(self.docker.check_applicable())
+
+
+class TestUpdateDependencies(BaseTest):
+    def test_update(self) -> None:
+        self.docker.update_dependencies()
+
+
+class TestReadDockerfile(BaseTest):
+    def test_read(self) -> None:
+        self.docker.read_dockerfile()
+
+
+class TestAttemptUpdateImage(BaseTest):
+    def test_attempt(self) -> None:
+        self.docker.attempt_update_image('')
+
+
+class TestCommitDockerfile(BaseTest):
+    def test_commit(self) -> None:
+        self.docker.commit_dockerfile([])
