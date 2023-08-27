34f38bee5d300c829c52f1dc7e809d49cb86ee29
Albert Wang <git@albertyw.com>
2023-08-27 16:04:39 -0400

    Use single quotes instead of double quotes

--- req_update/tests/test_cli.py
+++ req_update/tests/test_cli.py
@@ -9,20 +9,20 @@ class TestCLI(unittest.TestCase):
     def setUp(self) -> None:
         current = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
         parent = current.parent.resolve()
-        self.req_update_path = parent / "req_update.py"
+        self.req_update_path = parent / 'req_update.py'
         self.util = util.Util()
 
-    @unittest.skipUnless(os.environ.get("CI"), "Optimize to run only in CI")
+    @unittest.skipUnless(os.environ.get('CI'), 'Optimize to run only in CI')
     def test_cli(self) -> None:
-        command = ["python3", str(self.req_update_path), "-d"]
+        command = ['python3', str(self.req_update_path), '-d']
         self.util.execute_shell(command, readonly=True)
 
-    @unittest.skipUnless(os.environ.get("CI"), "Optimize to run only in CI")
+    @unittest.skipUnless(os.environ.get('CI'), 'Optimize to run only in CI')
     def test_shebang(self) -> None:
-        command = [str(self.req_update_path), "-d"]
+        command = [str(self.req_update_path), '-d']
         self.util.execute_shell(command, readonly=True)
 
-    @unittest.skipUnless(os.environ.get("CI"), "Optimize to run only in CI")
+    @unittest.skipUnless(os.environ.get('CI'), 'Optimize to run only in CI')
     def test_module(self) -> None:
-        command = ["python3", "-m", "req_update.req_update", "-d"]
+        command = ['python3', '-m', 'req_update.req_update', '-d']
         self.util.execute_shell(command, readonly=True)
