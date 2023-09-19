81cb1e68a3e0cfe082913177a2f5a1a77e3a4de6
Albert Wang <git@albertyw.com>
2023-09-18 23:08:18 -0400

    Deduplicate drone and docker files

--- req_update/drone.py
+++ req_update/drone.py
@@ -6,32 +6,3 @@ from req_update.docker import Docker
 class Drone(Docker):
     UPDATE_FILE = '.drone.yml'
     LINE_HEADER = 'image:'
-
-    def update_dependencies(self) -> bool:
-        """
-        Update dependencies
-        Return if updates were made
-        """
-        drone_lines = self.read_update_file()
-        updates = False
-        for i in range(len(drone_lines)):
-            line = drone_lines[i]
-            new_line, dependency, version = self.attempt_update_image(line)
-            if not dependency or not version:
-                continue
-            updates = True
-            drone_lines[i] = new_line
-            self.commit_drone(drone_lines, dependency, version)
-        if not updates:
-            self.util.warn('No %s updates' % self.language)
-        return updates
-
-    def commit_drone(self,
-        drone_lines: list[str],
-        dependency: str,
-        version: str
-    ) -> None:
-        if not self.util.dry_run:
-            with open(self.UPDATE_FILE, 'w') as handle:
-                handle.write('\n'.join(drone_lines))
-        self.util.commit_dependency_update(self.language, dependency, version)
