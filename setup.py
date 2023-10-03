3f73a6e6ddba2ed63b78569327294fd90d738c3d
Albert Wang <git@albertyw.com>
2023-10-02 23:03:15 -0700

    Update package to production status

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author_email='git@albertyw.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Topic :: Software Development :: Version Control',
