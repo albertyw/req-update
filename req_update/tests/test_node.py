39d9a379d3534e5dcea1e03b5cf5b4f686dac4df
Albert Wang <git@albertyw.com>
2025-11-29 19:14:25 -0800

    Remove redundant tests

--- req_update/tests/test_node.py
+++ req_update/tests/test_node.py
@@ -32,32 +32,6 @@ class TestCheckApplicable(unittest.TestCase):
         setattr(self.node.util, 'execute_shell', self.mock_execute_shell)
 
     @patch('os.listdir')
-    def test_applicable(self, mock_listdir: MagicMock) -> None:
-        def execute_shell_returns(
-            command: list[str],
-            readonly: bool,
-            suppress_output: bool,
-        ) -> subprocess.CompletedProcess[bytes]:
-            return MagicMock(stdout='')
-
-        self.mock_execute_shell.side_effect = execute_shell_returns
-        mock_listdir.return_value = ['package.json', 'package-lock.json']
-        applicable = self.node.check_applicable()
-        self.assertTrue(applicable)
-
-    def test_no_npm(self) -> None:
-        def execute_shell_returns(
-            command: list[str],
-            readonly: bool,
-            suppress_output: bool,
-        ) -> subprocess.CompletedProcess[bytes]:
-            raise subprocess.CalledProcessError(1, 'asdf')
-
-        self.mock_execute_shell.side_effect = execute_shell_returns
-        applicable = self.node.check_applicable()
-        self.assertFalse(applicable)
-
-    @patch('os.listdir')
     def test_no_package(self, mock_listdir: MagicMock) -> None:
         def execute_shell_returns(
             command: list[str],
